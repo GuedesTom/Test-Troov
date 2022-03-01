5c1039dfecf074b569e08b50b49532b5b60cfdfd
GuedesTom <92975112+GuedesTom@users.noreply.github.com>
2022-03-01 10:02:30 +0100

    Ajout Mongo URI / index.js

---
+++ back/index.js
@@ -0,0 +1,30 @@
+//serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+let express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Serveur running on port ${process.env.PORT}`)
+);
